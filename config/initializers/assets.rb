5a9aa6498ad92ebee9d87434b050586ae3160100
goodmountain37 <goodmountain37@gmail.com>
2020-08-15 20:15:45 +0900

    [add] temporary top page

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,5 @@
 Rails.application.config.assets.version = "1.0"
 Rails.application.config.assets.paths << Rails.root.join("node_modules")
 Rails.application.config.assets.precompile += %W( staff.css )
+Rails.application.config.assets.precompile += %W( admin.css )
+Rails.application.config.assets.precompile += %W( customer.css )
