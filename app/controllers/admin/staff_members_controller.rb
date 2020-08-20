4ca3e9bb7601ca41c20a876b605546520a588c70
goodmountain37 <goodmountain37@gmail.com>
2020-08-20 11:31:34 +0900

    [add] kaminari

--- app/controllers/admin/staff_members_controller.rb
+++ app/controllers/admin/staff_members_controller.rb
@@ -1,8 +1,7 @@
 class Admin::StaffMembersController < Admin::Base
-  before_action :authorize
-
   def index
     @staff_members = StaffMember.order(:family_name_kana, :given_name_kana)
+      .page(params[:page])
   end
 
   def show
